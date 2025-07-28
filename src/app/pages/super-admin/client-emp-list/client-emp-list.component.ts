5225ad2276bc3a7fae9696d273a7efd57f598bb9
Ananthu <ananthualpd@gmail.com>
2025-07-28 21:48:34 +0530

    emp dashboard beta added

---
+++ src/app/pages/super-admin/client-emp-list/client-emp-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-emp-list',
+  templateUrl: './client-emp-list.component.html',
+  styleUrls: ['./client-emp-list.component.scss']
+})
+export class ClientEmpListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
